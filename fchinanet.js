e0fb16ed9f9e08d86a20721f65411969748e728e
Anapopo <favoursmoe@gmail.com>
2018-03-18 17:43:53 +0800

    利用Node重制函数名称部分

---
+++ fchinanet.js
@@ -0,0 +1,37 @@
+function saveUser() {}
+
+function updateUser(name, content) {}
+
+function getUser() {}
+
+function newClient() {}
+
+function checkErr(err, info) {}
+
+function login() {}
+
+function checkEncry() {}
+
+function checkNet() {}
+
+function initial() {}
+
+function loginChinaNet() {}
+
+function checkLogin(result, full) {}
+
+function kickOffDevice(ip, brasIp) {}
+
+function getPasswd() {}
+
+function getQrCode() {}
+
+function online() {}
+
+function createUserFile() {}
+
+function checkXML() {}
+
+function menu() {}
+
+function main() {}
